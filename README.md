1ed10e3f6ee2dde207faee6710000115c63fa699
jiang1997 <jieke@live.cn>
2022-08-11 15:11:53 +0800

    support json with serde_json

--- README.md
+++ README.md
@@ -24,8 +24,37 @@ make run
 
 output
 ```
-module a, message: call after first load
-from f1, implemented in host
-module b, message: call after seconed load
-from f1, implemented in host
+enter module a, message: "call after first load"
+enter host f1, message: "implemented in host"
+
+enter module b, message: "call after second load"
+enter host f1, message: "implemented in host"
+
+enter module b
+Hi, "John Doe"
+Now, you are 43 old
+You must be 48 old in 2027
+```
+
+# 函数模板
+## host side
+```rust
+fn f1(v: &serde_json::Value) -> serde_json::Value {
+    println!("enter host f1, message: {}", v["message"]);
+    println!("");
+    let rs = json!({
+        "message": "ok",
+      });
+    rs
+}
+```
+
+## module side
+```rust
+fn modulef1(s: &serde_json::Value) -> serde_json::Value{
+    println!("enter module b, message: {}", s["message"]);
+    // call func from host side
+    imports::proxy("f1", &json!({"message": "implemented in host",}).to_string());
+    json!({"message": "modulef1",})
+}
 ```
--- host/src/main.rs
+++ host/src/main.rs
@@ -1,9 +1,13 @@
 use std::{collections::HashMap, hash::Hash};
 use lazy_static::*;
+use serde_json::json;
 use std::sync::Mutex;
 // use anyhow::Context;
 use anyhow::Result;
 use wasmtime::{*};
+
+use serde::{Deserialize, Serialize};
+// use serde_json::Result;
 
 wit_bindgen_wasmtime::export!("../imports.wit");
 wit_bindgen_wasmtime::import!("../exports.wit");
@@ -14,19 +18,29 @@ use exports::*;
 
 
 // ----------------- 实际实现 --------------
-fn f1(s: &String) -> &String {
-    println!("from f1, {}", s);
-    s
+fn f1(v: &serde_json::Value) -> serde_json::Value {
+    println!("enter host f1, message: {}", v["message"]);
+    println!("");
+    let rs = json!({
+        "message": "ok",
+      });
+    rs
 }
 
 
-fn f2(s: &String) -> &String {
-    println!("from f2, {}", s);
-    s
+fn f2(v: &serde_json::Value) -> serde_json::Value {
+    println!("enter host f2, message: {}", v["message"]);
+    println!("");
+    let rs = json!({
+        "message": "ok",
+      });
+    rs
 }
 
-fn f3() {
+fn f3(v: &serde_json::Value) -> serde_json::Value {
     println!("f3");
+    println!("");
+    serde_json::from_str("{}").unwrap()
 }
 
 // -----------------------------------------
@@ -38,7 +52,7 @@ pub struct MyImports;
 
 // type FuncType = fn(&String)->&String;
 lazy_static! {
-    static ref HASHMAP: Mutex<HashMap<String, fn(&String)->&String>> = {
+    static ref HASHMAP: Mutex<HashMap<String, fn(&serde_json::Value)->serde_json::Value>> = {
         let mut m = HashMap::new();
         Mutex::new(m)
     };
@@ -109,7 +123,7 @@ fn instantiate<I: Default, E: Default, T>(
 }
 
 
-fn registry(name: &str, f: fn(&String)->&String) {
+fn registry(name: &str, f: fn(&serde_json::Value)->serde_json::Value) {
     {
         let mut map = HASHMAP.lock().unwrap();
         map.insert(String::from(name), f);
@@ -129,19 +143,29 @@ fn registry_module(path: &str, name: &str) -> Result<()> {
     Ok(())
 }
 
-fn call_module_func(mname: &str, fname: &str, param: &str) -> String {
+fn call_module_func(mname: &str, fname: &str, param: &serde_json::Value) -> serde_json::Value {
 
     let mut map = MODULE_FUNC.lock().unwrap();
     let (e, mut s) = map.remove(mname).unwrap();
-    let rs = e.proxy(&mut s, fname, param);      
+    let rs = e.proxy(&mut s, fname, &param.to_string());      
     map.insert(String::from(mname), (e, s));
-
-    rs.unwrap()
+    // rs.unwrap()
+    let v:serde_json::Value = serde_json::from_str(rs.unwrap().as_str()).unwrap();
+    v
 }
 
 fn main() -> Result<()> {
     registry("f1", f1);
     registry("f2", f2);
+
+    let john = json!({
+      "name": "John Doe",
+      "age": 43,
+      "phones": [
+          "+44 1234567",
+          "+44 2345678"
+      ]
+    });
 
     impl Imports for MyImports {
         // 暴露给wasm module的函数
@@ -149,20 +173,22 @@ fn main() -> Result<()> {
         fn proxy(&mut self, name: &str, param: &str) -> String {
             let mut map = HASHMAP.lock().unwrap();
             let param = String::from(param);
-
-            let rs = map.get(name).unwrap()(&param);
-            "sd".into()
+            let v:serde_json::Value = serde_json::from_str(&param).unwrap();
+            let rs = map.get(name).unwrap()(&v);
+            rs.to_string()
         }
-
     }
 
     // 加载 “module_A.wasm” ，并注册为 “module_A”
     registry_module("module_A.wasm", "module_A");
-    call_module_func("module_A", "modulef1", "call after first load");
+    call_module_func("module_A", "modulef1", &json!({"message": "call after first load",}));
 
     // 加载 “module_B.wasm” ，并注册为 “module_A”
     registry_module("module_B.wasm", "module_A");
-    call_module_func("module_A", "modulef1", "call after seconed load");
+    call_module_func("module_A", "modulef1", &json!({"message": "call after second load",}));
+
+
+    let rs = call_module_func("module_A", "modulef2", &john);
 
 
     Ok(())
